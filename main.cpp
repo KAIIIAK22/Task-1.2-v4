fcde49a1fa0f9b5b2e82d3384aa71ccd2c19cc67
Pavel Poltaves <dzhura17@yandex.ru>
2019-08-02 18:43:25 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,10 +2,10 @@
 using namespace std;
 
    int main()
-   {
-    unsigned  int a ;
-    cout<<"Vasha zarplata($): ";
-    cin >>a;
-    cout <<(a>1000?(a>1000000?"Ti millioner!":"Horosho"):"Rabotai bolshe")<<"...no ti molodec";
-    return 0;
-   }
+    {
+      unsigned  int a ;
+      cout<<"Vasha zarplata($): ";
+      cin >>a;
+      cout <<(a>1000?(a>1000000?"Ti millioner!":"Horosho"):"Rabotai bolshe")<<"...no ti molodec";
+      return 0;
+    }
